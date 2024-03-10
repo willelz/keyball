f17bac1a2220bca025ab67dabcf326798d1f094e
willelz <37827548+willelz@users.noreply.github.com>
2024-03-10 23:04:26 +0900

    setup

--- qmk_firmware/keyboards/keyball/keyball61/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/config.h
@@ -29,8 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Split parameters
 #define SOFT_SERIAL_PIN         D2
 #define SPLIT_HAND_MATRIX_GRID  F7, D7
-#define SPLIT_USB_DETECT
-#define SPLIT_USB_TIMEOUT       500
+#define MASTER_LEFT
 
 #define SPLIT_TRANSACTION_IDS_KB KEYBALL_GET_INFO, KEYBALL_GET_MOTION, KEYBALL_SET_CPI
 
